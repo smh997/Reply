c2fd00b030d52f9ec54774c2597011e81107dd98
smh10 <smh10.mh@gmail.com>
2024-12-10 15:37:55 -0500

    The foundation to use WindowSizeClass values to change the layout for making it adapt to different screen sizes is established.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
 
+    implementation("androidx.compose.material3:material3-window-size-class")
+
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/reply/ui/ReplyApp.kt
+++ app/src/main/java/com/example/reply/ui/ReplyApp.kt
@@ -15,6 +15,7 @@
  */
 package com.example.reply.ui
 
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -24,11 +25,26 @@ import com.example.reply.data.MailboxType
 
 @Composable
 fun ReplyApp(
-    modifier: Modifier = Modifier,
+    windowSize: WindowWidthSizeClass,
+    modifier: Modifier = Modifier
 ) {
     val viewModel: ReplyViewModel = viewModel()
     val replyUiState = viewModel.uiState.collectAsState().value
 
+    when(windowSize){
+        WindowWidthSizeClass.Compact -> {
+
+        }
+        WindowWidthSizeClass.Medium -> {
+
+        }
+        WindowWidthSizeClass.Expanded -> {
+
+        }
+        else -> {
+
+        }
+    }
     ReplyHomeScreen(
         replyUiState = replyUiState,
         onTabPressed = { mailboxType: MailboxType ->
