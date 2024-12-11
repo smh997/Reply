64cfc9b6313db01dee1991f63755df1fb6641593
smh10 <smh10.mh@gmail.com>
2024-12-11 13:26:56 -0500

    A test for the compact screen size is added to check bottom navigation exists.

---
+++ app/src/androidTest/java/com/example/reply/test/ReplyAppTest.kt
@@ -0,0 +1,27 @@
+package com.example.reply.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.example.reply.R
+import com.example.reply.ui.ReplyApp
+import org.junit.Rule
+import org.junit.Test
+
+
+class ReplyAppTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun compactDevice_verifyUsingBottomNavigation() {
+        composeTestRule.setContent {
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Compact
+            )
+        }
+        composeTestRule.onNodeWithTagForStringId(R.string.navigation_bottom).assertExists()
+    }
+
+}
--- app/src/main/java/com/example/reply/ui/ReplyHomeScreen.kt
+++ app/src/main/java/com/example/reply/ui/ReplyHomeScreen.kt
@@ -196,6 +196,7 @@ private fun ReplyAppContent(
                     navigationItemContentList = navigationItemContentList,
                     modifier = Modifier
                         .fillMaxWidth()
+                        .testTag(bottomNavigationContentDescription)
                 )
             }
         }
